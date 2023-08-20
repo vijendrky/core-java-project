30652b35c38569945ed59f0e0ed054ffb697a72e
vijendra <vijendraky@gmail.com>
2023-08-20 20:27:59 +0530

    2nd java practice program

--- java-practice-session/src/com/core/java/chapter_01/tutorial_01/HelloWorld.java
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/HelloWorld.java
@@ -2,7 +2,7 @@ package com.core.java.chapter_01.tutorial_01;
 
 /**
  * Created by vijendra yadav on 20/08/2023.
- * Tutorial: Your first Java Hello World application
+ * Tutorial: My first Java Hello World application
  * [[<a href="http://vijendra.yadav.com/tutorials/chapter-1/hello-world">...</a>]]
  *
  */
---
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/HelloWorldWithArguments.java
@@ -0,0 +1,25 @@
+package com.core.java.chapter_01.tutorial_01;
+/**
+ * Created by vijendra yadav on 20/08/2023.
+ * Tutorial: Hello World application with program arguments.
+ * [[<a href="http://vijendra.yadav.com/tutorials/chapter-1/hello-world-with-arguments">...</a>]]
+ *
+ */
+public class HelloWorldWithArguments {
+    public static void main(String[] args) {
+        System.out.println("Hello World With Arguments Java Application!");
+
+        System.out.println("first argument : "+ args[0]);
+        System.out.println("second argument : "+ args[1]);
+
+        System.out.print(args[0]+" ");
+        System.out.print(args[1]+" ");
+        System.out.print(args[2]+" ");
+        System.out.print(args[3]+" ");
+        System.out.print(args[4]);
+
+
+
+
+    }
+}
