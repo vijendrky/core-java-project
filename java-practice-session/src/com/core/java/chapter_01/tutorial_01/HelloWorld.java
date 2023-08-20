1dd8bd7da5a2cfedca4d3b7cb1efaa5ac43aefa9
vijendra <vijendraky@gmail.com>
2023-08-20 20:16:37 +0530

    first java program

---
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.core.java.chapter_01.tutorial_01;
+
+/**
+ * Created by vijendra yadav on 20/08/2023.
+ * Tutorial: Your first Java Hello World application
+ * [[<a href="http://vijendra.yadav.com/tutorials/chapter-1/hello-world">...</a>]]
+ *
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+    }
+}
