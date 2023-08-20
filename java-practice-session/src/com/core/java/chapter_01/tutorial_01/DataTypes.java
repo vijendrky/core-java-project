c6521ac16e6acda6fc6952e130b7aceba7c07c6b
vijendra <vijendraky@gmail.com>
2023-08-20 22:13:58 +0530

    code modifid

--- java-practice-session/src/com/core/java/chapter_01/tutorial_01/DataTypes.java
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/DataTypes.java
@@ -10,10 +10,14 @@ public class DataTypes {
         long b = 20;
         System.out.println("long ="+ b);
         int a = 10;
+        System.out.println("integer =" +a);
         short s =5;
+        System.out.println("short ="+ s);
         byte c = 1;
+        System.out.println("Byte =" +c);
 
         double d =2.50;
+        System.out.println(d);
         float f = 16.9f;
 
         char ch ='a';
