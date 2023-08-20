735bc6484e63befb9e5e8ad40a4c505ff12813f5
vijendra <vijendraky@gmail.com>
2023-08-20 21:58:07 +0530

    data type code commited

---
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/DataTypes.java
@@ -0,0 +1,25 @@
+package com.core.java.chapter_01.tutorial_01;
+/**
+ * Created by vijendra yadav on 20/08/2023.
+ * Tutorial: Java Data types tutorials
+ * [[<a href="http://vijendra.yadav.com/tutorials/chapter-1/data -types">...</a>]]
+ *
+ */
+public class DataTypes {
+    public static void main(String[] args) {
+        long b = 20;
+        System.out.println("long ="+ b);
+        int a = 10;
+        short s =5;
+        byte c = 1;
+
+        double d =2.50;
+        float f = 16.9f;
+
+        char ch ='a';
+        boolean isTrue = true;
+
+        System.out.println("boolean data type : "+isTrue);
+
+    }
+}
