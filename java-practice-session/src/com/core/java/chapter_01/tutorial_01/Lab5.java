0f56ea3c9f78d1162fc3485bbea738d231d512ec
vijendra <vijendraky@gmail.com>
2023-08-21 11:49:21 +0530

    Lab5

---
+++ java-practice-session/src/com/core/java/chapter_01/tutorial_01/Lab5.java
@@ -0,0 +1,20 @@
+package com.core.java.chapter_01.tutorial_01;
+
+public class Lab5 {
+    public static void main(String[] args) {
+        String str1 = "OK";
+        String str2 = "JLC";
+        String  str3 = str1 + str2 ;
+        System.out.println(str3);
+        int ab = 10;
+        String st4 = str1 + ab;
+        System.out.println(st4);
+        String str5 = str1 +11234.5;
+        System.out.println(str5);
+        String str6 =  11234.5 + str1;
+        System.out.println(str6);
+        System.out.println(90+10);
+        System.out.println("result is "  + 90+10);
+        
+    }
+}
